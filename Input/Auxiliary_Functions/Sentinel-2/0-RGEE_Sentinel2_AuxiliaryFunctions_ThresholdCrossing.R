0ad4d11f0e8b1f031637226a3116f9bb8dee9c56
TVersluijs <tom.versluijs@gmail.com>
2023-10-31 15:36:47 +0100

    Updated Threshold crossing functions for MODIS and Sentinel-2 to accommodate multiple threshold levels at once.

--- Input/Auxiliary_Functions/Sentinel-2/0-RGEE_Sentinel2_AuxiliaryFunctions_ThresholdCrossing.R
+++ Input/Auxiliary_Functions/Sentinel-2/0-RGEE_Sentinel2_AuxiliaryFunctions_ThresholdCrossing.R
@@ -24,7 +24,7 @@
 
 #(I): Specify function to define the moment of crossing of the threshold
 f_detect_threshold_date_parallel <- function(subset, pixelIDs_split=pixelIDs_split, df_pixel_y=df_pixel_ndsi, pixel_ID_column="pixel_ID",
-                                             y="NDSI", x="doy", pixel_gam_plots=T, y_threshold=NDSI_threshold){
+                                             y="NDSI", x="doy", pixel_gam_plots=T, y_threshold=NDSI_threshold_vector){
 
   #Select data for the current data subset
   df_subset <- df_pixel_y[df_pixel_y[,pixel_ID_column] %in% pixelIDs_split[[subset]],]
@@ -78,29 +78,30 @@ f_detect_threshold_date_parallel <- function(subset, pixelIDs_split=pixelIDs_spl
                        newdata <- base::data.frame(x=seq(min(df_tmp$x), max(df_tmp$x), 1))
                        newdata$gam_predict <- stats::predict(mod_gam, newdata=newdata, type="response")
 
-                       #Detect cutoff points for crossing the threshold from above to below the curve (e.g. snowmelt)
+                       #Detect for each threshold in y_threshold when this was crossed in newdata
+                       list_threshold <- lapply(y_threshold, function(y_threshold_i){
 
-                         #Detect cutoff points where curve goes from above y_threshold to below y_threshold
-                         newdata$cutoff <- ifelse(newdata$gam_predict >= y_threshold, 1, 0)
+                         #Detect cutoff points for crossing the threshold from above y_threshold_i to below y_threshold_i (e.g. snowmelt)
+                         newdata$cutoff <- ifelse(newdata$gam_predict >= y_threshold_i, 1, 0)
                          newdata$dif <- c(0, diff(newdata$cutoff))
-                         #the column 'cutoff' indicates whether the gam prediction is above (1) or below (0) the y_threshold
+                         #the column 'cutoff' indicates whether the gam prediction is above (1) or below (0) the y_threshold_i
                          #the column 'dif' indicates when there is a change from 1 to 0 (-1) or 0 to 1 (1) in the column cutoff
                          #Thus, those rows where the column 'dif' is equal to -1 indicate moments where the y value changes from above
                          #the threshold to below the threshold. It might be possible that this happens multiple times within a season due to
                          #measurement errors or cloud effects. We therefore need to determine which 'cutoff' most likely corresponds to the
                          #final moment of threshold crossing (e.g. the actual moment of snowmelt)
 
-                       #If y_threshold was at least crossed once:
-                       if(any(newdata$dif<0)){
+                         #If y_threshold_i was at least crossed once:
+                         if(any(newdata$dif<0)){
 
-                         #Select all moments (cutoffs) where dif==-1
-                         cutoffs <- data.frame(index=which(newdata$dif<0))
+                           #Select all moments (cutoffs) where dif==-1
+                           cutoffs <- data.frame(index=which(newdata$dif<0))
 
-                         #If direction of crossing was from above to below the threshold (e.g. snowmelt):
+                           #If direction of crossing was from above to below the threshold (e.g. snowmelt):
 
-                           #For the period 30 days after each cutoff point, sum the number of days that have a y value LARGERE than y_threshold.
+                           #For the period 30 days after each cutoff point, sum the number of days that have a y value LARGERE than y_threshold_i.
                            #If a cutoff represents the final moment the threshold is crossed, then we do not expect any days after this moment
-                           #with y > y_threshold. Thus, the closer this sum is to 0, the more likely this cutoff corresponds to the final moment
+                           #with y > y_threshold_i. Thus, the closer this sum is to 0, the more likely this cutoff corresponds to the final moment
                            #that this threshold is crossed (i.e. the actual moment of snowmelt).
                            cutoffs$min <- cutoffs$index -30
                            cutoffs$min[cutoffs$min<1] <- 1
@@ -109,53 +110,66 @@ f_detect_threshold_date_parallel <- function(subset, pixelIDs_split=pixelIDs_spl
                            cutoffs$sum_cutoff_plus_30 <- apply(cutoffs, 1, function(x){sum(newdata$cutoff[x['index']:(x['max'])])})
                            cutoff_best <- cutoffs[cutoffs$sum_cutoff_plus_30==min(cutoffs$sum_cutoff_plus_30),'index'][1]
 
-                         #Approximate day on which threshold is crossed in period from (cutoff_best-1 : cutoff_best)
-                         newdata_subset <- newdata[max(0, cutoff_best-2) : min(cutoff_best+1, nrow(newdata)),]
-                         x_threshold <- stats::approx(x = newdata_subset$gam_predict, y = newdata_subset$x, xout = y_threshold)$y[1]
+                           #Approximate day on which threshold is crossed in period from (cutoff_best-1 : cutoff_best)
+                           newdata_subset <- newdata[max(0, cutoff_best-2) : min(cutoff_best+1, nrow(newdata)),]
+                           x_threshold <- stats::approx(x = newdata_subset$gam_predict, y = newdata_subset$x, xout = y_threshold_i)$y[1]
 
-                         #Create plot of GAM
-                         if(pixel_gam_plots==T){
+                         }
+                         #If threshold was not crossed:
+                         if(!any(newdata$dif<0)){
+
+                           #There was no date at which the threshold was crossed (set x_threshold to NA)
+                           x_threshold <- NA
+
+                         }
+
+                         #Store date at which threshold was crossed (e.g. date of snowmelt in case of y=NDSI) for each pixel in dataframe:
+                         df_pixel_threshold <- base::data.frame(pixel_ID=pixel, y_threshold=y_threshold_i, x_threshold=x_threshold)
+                         return(df_pixel_threshold)
+
+                       })
+
+                       #Store threshold crossing as dataframe
+                       df_pixel_threshold <- as.data.frame(do.call(rbind, list_threshold))
+
+                       #Plot data
+                       if(pixel_gam_plots==T){
+
+                         #if at least one threshold crossing was detected:
+                         if(any(!is.na(df_pixel_threshold$x_threshold))){
+
                            p_tmp <- ggplot2::ggplot()+
                              geom_point(data=df_tmp[index,], aes(x=x, y=y))+
                              geom_point(data=df_tmp[df_tmp$outliers==TRUE,], aes(x=x, y=y), col="black", pch=16, alpha=0.2)+
                              geom_line(data=newdata, aes(x=x, y=gam_predict), col = "red") +
-                             geom_point(aes(x=x_threshold, y=y_threshold), col="blue", size=3)+
-                             geom_hline(yintercept=y_threshold, lty=2, col="grey")+
+                             geom_point(data=df_pixel_threshold, aes(x=x_threshold, y=y_threshold), col="blue", size=3)+
+                             geom_hline(data=df_pixel_threshold, aes(yintercept=y_threshold), lty=2, col="grey")+
                              xlab(paste0("Day of year (starting at 01-01-", year_ID, ")")) +
                              ylab(paste0(y, "-value at pixel")) +
                              theme_classic()+
                              ggtitle(pixel)
-                             }
-                         if(pixel_gam_plots==F){
-                           p_tmp <- NULL
-                           }
 
-                       #If threshold was not crossed:
-                       if(!any(newdata$dif<0)){
+                         }
 
-                         #There was no date at which the threshold was crossed (set x_threshold to NA)
-                         x_threshold <- NA
+                         #If no threshold crossing was detected:
+                         if(!any(!is.na(df_pixel_threshold$x_threshold))){
 
-                         #Create plot of GAM
-                         if(pixel_gam_plots==T){
                            p_tmp <- ggplot2::ggplot()+
                              geom_point(data=df_tmp[index,], aes(x=x, y=y))+
                              geom_point(data=df_tmp[df_tmp$outliers==TRUE,], aes(x=x, y=y), col="black", pch=16, alpha=0.2)+
                              geom_line(data=newdata, aes(x=x, y=gam_predict), col = "red") +
-                             geom_hline(yintercept=y_threshold, lty=2, col="grey")+
+                             geom_hline(data=df_pixel_threshold, aes(yintercept=y_threshold), lty=2, col="grey")+
                              xlab(paste0("Day of year (starting at 01-01-", year_ID, ")")) +
                              ylab(paste0(y, "-value at pixel")) +
                              theme_classic()+
                              ggtitle(pixel)
-                         }
-                         if(pixel_gam_plots==F){
-                           p_tmp <- NULL
-                           }
 
                          }
 
-                       #Store date at which threshold was crossed (e.g. date of snowmelt in case of y=NDSI) for each pixel in dataframe:
-                       df_pixel_threshold <- base::data.frame(pixel_ID=pixel, x_threshold=x_threshold)
+                       }
+                       if(pixel_gam_plots==F){
+                         p_tmp <- NULL
+                       }
 
                        #Store df_pixel_threshold and plot df_tmp in the multiresultclass object
                        result <- list(df_pixel_threshold, p_tmp)
@@ -167,5 +181,4 @@ f_detect_threshold_date_parallel <- function(subset, pixelIDs_split=pixelIDs_spl
 
                      }
 
-  }
 
