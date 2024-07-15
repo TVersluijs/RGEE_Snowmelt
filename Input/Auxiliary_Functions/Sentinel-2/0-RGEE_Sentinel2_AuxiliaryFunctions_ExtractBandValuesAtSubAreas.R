deb9132daf51c383ccbe8c5538ffdf96152a8d26
TVersluijs <tom.versluijs@gmail.com>
2024-07-15 14:01:17 +0200

    Minor update: -Changed the usage of 'SubAreas' to 'Polygons' in auxiliary script ...ExtractBandValuesAtSubAreas.R

--- Input/Auxiliary_Functions/Sentinel-2/0-RGEE_Sentinel2_AuxiliaryFunctions_ExtractBandValuesAtSubAreas.R
+++ Input/Auxiliary_Functions/Sentinel-2/0-RGEE_Sentinel2_AuxiliaryFunctions_ExtractBandValuesAtSubAreas.R
@@ -1,44 +1,45 @@
 ####################################################################################################################################
 #
-#                              Sentinel 2 - Extract band values at SubAreas within the area of interest
+#                              Sentinel 2 - Extract band values at Polygons within the area of interest
 #
 ####################################################################################################################################
 
-#DESCRIPTION: The function Extract_BandValuesAtSubAreas() can be used to iterate through all images of a Sentinel-2 image collection.
-#For each image, the value of the band of interest (e.g. NDSI) is extracted for each SubArea within the feature collection aoi_SubAreas.
-#The resulting SubArea-specific band values are subsequently stored as feature properties in a feature collection together with
-#the datetime of the image. Within this feature collection, each SubArea thus has a separate feature with its properties. This
-#results in a feature collection of all SubAreas for the current image. This feature collection is then appended to a list of feature
-#collections from previous images in the iteration. Thus, at each iteration we extract band values of interest for all SubAreas within
+#DESCRIPTION: The function Extract_BandValuesAtPolygons() can be used to iterate through all images of a Sentinel-2 image collection.
+#For each image, the value of the band of interest (e.g. NDSI) is extracted for each polygon within the feature collection aoi_Polygons.
+#The resulting polygon-specific band values are subsequently stored as feature properties in a feature collection together with
+#the datetime of the image. Within this feature collection, each polygon thus has a separate feature with its properties. This
+#results in a feature collection for all polygons for the current image. This feature collection is then appended to a list of feature
+#collections from previous images in the iteration. Thus, at each iteration we extract band values of interest for all polygons within
 #the current image, store this as a  feature collection and add this to an expanding list of feature collections from previous iterations.
 
-#In addition, based on the binary band "SNOW" in each image the fraction of snow-covered pixels is calculated by dividing the total
-#number of snow-covered pixels by the total number of pixels within the SubArea. This excludes any pixels that are masked due to e.g.
-#cloud cover. Thus, note that this SNOW band and masking of unwanted pixels should occur before running this function.
+#In addition (currently not activated as code is commented), based on the binary band "SNOW" in each image the fraction of snow-covered
+#pixels is calculated by dividing the total number of snow-covered pixels by the total number of pixels within the polygon. This excludes 
+#any pixels that are masked due to e.g. cloud cover. Thus, note that this SNOW band and masking of unwanted pixels should occur before 
+#running this function.
 
-#The function Extract_BandValuesAtSubAreas() takes two arguments. The first argument is the current element of the image collection
+#The function Extract_BandValuesAtPolygons() takes two arguments. The first argument is the current element of the image collection
 #(i.e. the current image within the iteration) and the second element takes the output FeatureCollection from the iteration that
 #preceded it. The latter is not possible for the first iteration, that's why an initial empty feature collection to start the
 #iteration with must be defined.
 
-#Note that inside $map() functions all processing has to be done in the language of the server (javascript Api of google earth
-#engine). Thus, inside $map() functions, client-side functions such as $getInfo() or print(), paste0() cannot be used.
+#Note that inside $map() functions all processing has to be done in the language of the server (javascript Api of Google Earth
+#Engine). Thus, inside $map() functions, client-side functions such as $getInfo() or print(), paste0() cannot be used.
 
 ####################################################################################################################################
 
-#(I): Function to extract BandValues for all SubAreas within an image and store these values in an expanding Feature Collection:
-Extract_BandValuesAtSubAreas = function(img, FC_initial) {
+#(I): Function to extract BandValues for all Polygons within an image and store these values in an expanding Feature Collection:
+Extract_BandValuesAtPolygons = function(img, FC_initial) {
 
     ##For debugging
     #img <- s2_snow_masked$select("FSC_Gascoin2020", "FSC_Aalstad2020", NDSI", "NDVI", "NDMI", "SNOW")$filterDate(paste0(year_ID, "-06-05"), paste0(year_ID, "-07-31"))$first()
 
-    #(A): Calculate the mean NDSI, NDVI, NDMI and SNOW value for the current image within each subarea in the aoi_SubAreas feature collection.
-    #We add these mean values as properties to each feature (i.e. sub area). The resulting output is a feature collection for the current
-    #image where these means are added as properties for each sub area (i.e. different features). Note that taking the mean of the binary
-    #SNOW band results in a fraction of snow covered pixels for each image. ReduceRegions does not include masked pixels (i.e. pixels
-    #defined as cloud or water) when calculating these means.
-      FC_image <- img$reduceRegions(collection = aoi_SubAreas,
-                        reducer = ee$Reducer$mean(), #$setOutputs(list('NDSI'))
+    #(A): Calculate the mean NDSI, NDVI, NDMI and SNOW value for the current image within each Polygon in the aoi_Polygons feature collection.
+    #We add these mean values as properties to each feature (i.e. polygon). The resulting output is a feature collection for the current
+    #image where these means are added as properties for each polygon (i.e. different features). Note that taking the mean of the binary
+    #SNOW band results in a fraction of snow covered pixels for each image. The function reduceRegions() does not include masked pixels (i.e.
+    #pixels defined as cloud or water) when calculating these means.
+      FC_image <- img$reduceRegions(collection = aoi_Polygons,
+                        reducer = ee$Reducer$mean(),
                         scale = resolution,
                         crs=crs,
                         crsTransform=NULL)
@@ -49,21 +50,21 @@ Extract_BandValuesAtSubAreas = function(img, FC_initial) {
 
     #THE COMMENTED CODE BELOW SHOULD BE EQUIVALENT TO CALCULATING THE MEAN NUMBER OF PIXELS DEFINED AS SNOW COVERED AS DONE ABOVE
     #
-    # #(C): Add total number of pixels within each SubArea that are not masked by clouds nor water
+    # #(C): Add total number of pixels within each Polygon that are not masked by clouds nor water
     #
     #     #(I): Select band indicating which pixels are snow-covered (note that some of the pixels in these images might be cloud/water-masked):
     #      snow <- img$select('SNOW')
     #
-    #     #(II): Count number of pixels within each subarea that are not masked by clouds nor water (i.e. both snow covered pixels and pixels without snow cover):
+    #     #(II): Count number of pixels within each Polygon that are not masked by clouds nor water (i.e. both snow covered pixels and pixels without snow cover):
     #      TotalPixels <- snow$reduceRegions(
     #         reducer = ee$Reducer$count(),
-    #         collection = aoi_SubAreas,
+    #         collection = aoi_Polygons,
     #         scale = resolution,
     #         crs=crs,
     #         crsTransform=NULL
     #         )
     #
-    #     #(III): Only select 'Cluster_ID' (SubArea) and 'count' (renamed to Totalpixels) columns:
+    #     #(III): Only select 'Cluster_ID' (Polygon) and 'count' (renamed to Totalpixels) columns:
     #      TotalPixels = TotalPixels$map(function(feature){
     #        return(ee$Feature(NULL, list(TotalPixels = feature$get('count'), Cluster_ID = feature$get('Cluster_ID'))))
     #        })
@@ -88,7 +89,7 @@ Extract_BandValuesAtSubAreas = function(img, FC_initial) {
     #           return(f1$set(f2$toDictionary()))
     #           })
     #
-    # #(D): Add total number of snow-covered pixels within each subregion that are not masked by clouds nor water
+    # #(D): Add total number of snow-covered pixels within each polygon that are not masked by clouds nor water
     #
     #  #Note that all reducer functions do not include pixels where the mask is equal to 0. We have specified a cloud/water mask previously
     #  #to each image in the image collection, thus pixels containing clouds/water (with a mask equal to 0) are not used in the reduction
@@ -99,10 +100,10 @@ Extract_BandValuesAtSubAreas = function(img, FC_initial) {
     #     #(I): Mask pixels without snow (i.e. SNOW=1)
     #      snow <- snow$updateMask(img$select('SNOW')$gt(0.99))
     #
-    #     #(II): Count number of snow covered pixels within each subarea
+    #     #(II): Count number of snow covered pixels within each Polygon
     #      SnowyPixels <- snow$reduceRegions(
     #         reducer = ee$Reducer$count(),
-    #         collection = aoi_SubAreas,
+    #         collection = aoi_Polygons,
     #         scale = resolution,
     #         crs=crs,
     #         crsTransform=NULL
@@ -135,7 +136,7 @@ Extract_BandValuesAtSubAreas = function(img, FC_initial) {
 
     #(E): Make sure there is a value at each feature within the feature collection:
 
-      #reduceRegion does not return any band values when all pixels in the sub area are masked by clouds, or do not fall within
+      #reduceRegion does not return any band values when all pixels in the polygon are masked by clouds, or do not fall within
       #the boundaries of the image. Therefore we manually have to add an empty band property to those features.
       #We therefore set the band value to a no data value of -9999 for all features where the band value is NULL.
        FC_image <- FC_image$map(function(feature){
