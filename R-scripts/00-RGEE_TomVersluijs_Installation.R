9ffcd5c59833c39e979f4b9837617741e6d3b537
TVersluijs <tom.versluijs@gmail.com>
2025-06-14 15:47:09 +0200

    Commit to resolve synch issues

--- R-scripts/00-RGEE_TomVersluijs_Installation.R
+++ R-scripts/00-RGEE_TomVersluijs_Installation.R
@@ -2,7 +2,7 @@
 
 # Script: Script with installation procedures to use RGEE
 # Author: Tom Versluijs, adapted from Ricardo Dal'Agnol da Silva
-# Date Created: 2025-06-13
+# Date Created: 2025-06-14
 # R version 4.3.2
 # Adapted from the following video tutorial: https://www.youtube.com/watch?v=1-k6wNL2hlo&ab_channel=RicardoDalagnol
 
